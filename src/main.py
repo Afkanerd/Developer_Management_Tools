70f90b177e743d230231d56154c08ba4f12d7366
sh3rlock <wisdomnji@gmail.com>
2021-07-04 23:16:51 +0100

    update: examples in main of how to use tools in cli

--- src/main.py
+++ src/main.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 
 '''
+# scopes
 $ main create -scope <name>
 $ main delete -scope <name>
 
+# developers
 $ ID,AUTH_ID= main create -developer -scopes <scope_name> "<scope_mode>,"
 $ main create -developer -scopes "users" --modes "read, write"
 $ main create -developer -scopes "users" --modes "+rw"
 
-$ main update -developer <ID> <AUTH_ID> -scopes --modes "+r" "-r" "+r-w"
+$ 0,1= main update -developer <ID> <AUTH_ID> -scopes --modes "+r" "-r" "+r-w"
 
-$ main delete -developer <ID> <AUTH_ID>
+$ 0,1= main delete -developer <ID> <AUTH_ID>
+
+$ SCOPES= main fetch -developer <ID> <AUTH_ID>
 '''
