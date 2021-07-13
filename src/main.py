293ac8fa1bc0f1542564c0e69fdfdbf894899e51
sh3rlock <wisdomnji@gmail.com>
2021-07-13 16:27:33 +0100

    update: figured out cli parsing

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
 '''
-# scopes
-$ main create -scope <name>
+$ main create --scope [read, write, +rw]
+
+# creates users with default scope 'read'
+$ main create <name>
+
+
 $ main delete -scope <name>
 
 # developers
@@ -16,3 +20,26 @@ $ 0,1= main delete -developer <ID> <AUTH_ID>
 
 $ SCOPES= main fetch -developer <ID> <AUTH_ID>
 '''
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="cli args")
+    parser.add_argument("--scopes", help="Read (can fetch only), Write (can write and update)", default='read', const='read', choices=['read', 'write', '+rw'], nargs='?')
+    group = parser.add_argument_group('Actions', '')
+    group.add_argument("-create", help="create user with <email address> [--scopes]")
+    group.add_argument("-delete", help="delete user with <email address>")
+    group.add_argument("-update", help="update user with <email address> [--scopes]")
+    group.add_argument("-fetch", help="fetch user with  <email address>")
+
+    args = parser.parse_args()
+    print(args)
+
+    if args.create:
+        print(f'creating dev with scope [{args.scopes}]')
+    elif args.fetch:
+        print(f'fetching dev')
+    elif args.update:
+        print(f'updating dev')
+    elif args.delete:
+        print(f'deleting dev')
